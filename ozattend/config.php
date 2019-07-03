a66de1b7c30cdd625dcd47929780fb2e2a97c3ba
aaronLeague <48327594+aaronLeague@users.noreply.github.com>
2019-07-03 14:42:07 -0400

    Add files via upload

---
+++ ozattend/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//$host = "pi.cs.oswego.edu";
+$host = "pi.cs.oswego.edu";
+$dbusername = "ozattendf";
+$dbpassword = "csc380";
+$dbname = "ozattendance";
+// Create connection
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+?>
---
+++ ozattend/genrand.php
@@ -0,0 +1,12 @@
+<?php
+
+function genKey() {
+    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < 6; $i++) {
+        $randomString .= $characters[rand(0, 25)];
+    }
+    return $randomString;
+}
+
+?>
